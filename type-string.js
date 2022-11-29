6cf0fa5b5c94dd9b1134b32a524adf4271c64be9
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-11-29 16:01:40 -0300

    Update type-string.js

--- type-string.js
+++ type-string.js
@@ -5,3 +5,13 @@ const myNome = "Gustavo";
 
 console.log(texto2);
 console.log(texto1 + myNome);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
