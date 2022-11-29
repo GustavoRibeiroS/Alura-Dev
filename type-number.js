2b30cff0f600f3be4190d95422d25563aec2bd65
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-11-29 15:27:38 -0300

    testando Terminal no VS

---
+++ type-number.js
@@ -0,0 +1,19 @@
+const num1 = 3.3;
+const num2 = .5;
+
+const operacaoFeita = num1 / num2;
+
+console.log(operacaoFeita);
+
+const numInt1 = 1;
+const numInt2 = 2;
+
+const novaOperacao = numInt1 / num1
+
+console.log(novaOperacao);
+
+//NAN
+
+const aluno = "AlunoBeto";
+
+console.log(aluno * numInt1);
