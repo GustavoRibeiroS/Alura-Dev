434d8e86cae4cce705f8ef6afe0d6d93024f5426
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-11-29 16:12:02 -0300

    Create type-boolean.js

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+const num1 = 5;
+const num2 = 3;
+const num3 = 5;
+
+console.log(num1 === num2);
+console.log(num1 === num3);
+
+const tx1 = "Alunos";
+const tx2 = "Batata";
+
+console.log(tx1 === tx2);
+console.log(tx1.length === tx2.length);
