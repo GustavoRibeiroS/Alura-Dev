61b326659f3ecb171064e81d833f5d3f23874de2
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2022-12-06 16:00:51 -0300

    Introdução Arrays
    Introdução Arrays - Alura

--- media.js
+++ media.js
@@ -1,12 +1,13 @@
-const notas = [10, 6, 8];
-notas.push(7) //adicionando a nota 7
+const notas = [10, 6, 8, 5.5];
+notas.push(10) //adicionando a nota 10 no último Array
+notas.pop() //Deletou o último valor (10)
 function calcularMedia(notas) {
     let soma = 0;
 
     for (let i = 0; i < notas.length; i++) {
         soma += notas[i];
     }
-    console.log(`Valor da soma da media foi: ${soma}`);
+    console.log(`Valor da soma das notas foi: ${soma}`);
     let media = soma/notas.length;
     console.log(`A média do aluno é: ${media}`);
 
