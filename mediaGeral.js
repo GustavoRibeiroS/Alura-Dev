173eb4329dfaa8320f16c570a401cf8902db3c58
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2023-01-04 15:32:50 -0300

    Update mediaGeral.js

--- mediaGeral.js
+++ mediaGeral.js
@@ -3,7 +3,7 @@ const salaJava = [6, 5, 8, 9, 5, 6];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(notasSala) {
-    const somaDasNotas = notasSala.reduce((acumulador, nota) => { //acumulador recebe o valor 0 e na nota é atribuido os valores das salas que serão passadas como parâmetro
+    const somaDasNotas = notasSala.reduce((acumulador, nota) => { //acumulador recebe o valor 0 e na nota é atribuido os valores dos arrays das salas que serão passadas como parâmetro
         return acumulador + nota;
     },0) //Da mesma forma que se coloca uma variável para somar o array, ao 0 é atribuido está função (acumulador).
     console.log(`Soma notas: ${somaDasNotas}`);
