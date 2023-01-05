1933155cb6f4a516a7bc1cdbcfc24b91e57afdb4
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2023-01-05 14:58:52 -0300

    criando um objeto

---
+++ objetos-js/aula01.js
@@ -0,0 +1,9 @@
+// const informacoesPessoa = ["nome", "José", "idade", 22, "CPF", "44455522211"]
+// console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    nome: "Pedro",
+    idade: 32,
+};
+
+console.log(objetoPessoa);
