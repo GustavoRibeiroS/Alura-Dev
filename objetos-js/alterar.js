8af05c284fefea7fadc647328783d74d7b4616ad
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2023-01-05 15:34:53 -0300

    alterar propriedades e alterando valores

---
+++ objetos-js/alterar.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Larissa",
+    profissao: "Arquiteta"
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 955517565";
+console.log(pessoa.telefone);
+
+pessoa.nome = "Larissa Campos"
+console.log(pessoa);
