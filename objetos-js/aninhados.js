600c71a5a442e2b8726fdabb35d0c3de6eb04671
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2023-01-06 15:27:23 -0300

    aninhando informações JS

---
+++ objetos-js/aninhados.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@trab.com",
+    telefone: ["1122225555", "11925631458"],
+};
+
+cliente.endereco = {
+    rua: "R. Peascoal Freitas",
+    numero: 1253,
+    apartamento: true,
+    complemento: "Apto 52C"
+}
+console.log(cliente.endereco);
