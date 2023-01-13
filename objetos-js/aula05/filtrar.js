049f43812af94338ab02797ab4e454d36a7e89d3
Gustavo Ribeiro <gustavo.ribeiro1056@gmail.com>
2023-01-13 15:44:40 -0300

    método ordenar

--- objetos-js/aula05/filtrar.js
+++ objetos-js/aula05/filtrar.js
@@ -1,9 +1,9 @@
 const clientes = require('./clientes.json');
 
-function filtrarApartamentosCompletmentos(Clientes) {
-    return clientes.filter(cliente => {
+function filtrarApartamentosSemCompletmentos(clientes) {
+    return clientes.filter(cliente => { //Bool
         return cliente.endereco.apartamento && !cliente.endereco.hasOwnProperty("complemento") //filtra clientes que não tem a propriedade "complemento"
     })
 }
-const filtrados = filtrarApartamentosCompletmentos(clientes);
+const filtrados = filtrarApartamentosSemCompletmentos(clientes);
 console.log(filtrados);
